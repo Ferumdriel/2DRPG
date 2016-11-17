565c3742e94a4d566567f6318146aaf8942ddba4
Ferumdriel <Ferumdriel@gmail.com>
2016-11-18 00:29:52 +0100

    - only visible map is being rendered - map moving with character - Handler class created to get parameters of Game and World

---
+++ src/dev/Ferumdriel/tilegame/Main/Handler.java
@@ -0,0 +1,52 @@
+package dev.Ferumdriel.tilegame.Main;
+
+import dev.Ferumdriel.tilegame.gfx.GameCamera;
+import dev.Ferumdriel.tilegame.input.KeyManager;
+import dev.Ferumdriel.tilegame.worlds.World;
+
+/**
+ * Created by Binio on 2016-11-18.
+ */
+public class Handler {
+
+    private Game game;
+    private World world;
+
+    public Handler(Game game){
+        this.game = game;
+    }
+
+    //GETTERS AND SETTERS
+
+    public GameCamera getCamera(){
+        return game.getGameCamera();
+    }
+
+    public KeyManager getKeyManager(){
+        return game.getKeyManager();
+    }
+
+    public int getWidth(){
+        return game.getWidth();
+    }
+
+    public int getHeight(){
+        return game.getHeight();
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public World getWorld() {
+        return world;
+    }
+
+    public void setWorld(World world) {
+        this.world = world;
+    }
+}
